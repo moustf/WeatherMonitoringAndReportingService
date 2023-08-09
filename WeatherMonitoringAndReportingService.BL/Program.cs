871f087ba4506a0dbc085707c75f77367f84232f
Mustafa Salem <mustafaissasalem@gmail.com>
2023-08-10 00:42:22 +0300

    feat: create the project .net solution

---
+++ WeatherMonitoringAndReportingService.BL/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherMonitoringAndReportingService.BL
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, there!");
+            Console.WriteLine("This is the real-time weather monitoring and reporting service application!");
+        }
+    }
+}
